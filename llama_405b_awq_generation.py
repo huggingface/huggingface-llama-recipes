99270926bdaf420f18a48db592383d9319dd46c7
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2024-07-23 15:03:43 +0200

    Add `llama_405b_{awq,gptq}_generation.py` examples

---
+++ llama_405b_awq_generation.py
@@ -0,0 +1,32 @@
+# INSTALLATION
+# pip install "torch>=2.2.0,<2.3.0" "transformers[accelerate]" autoawq --upgrade
+# REQUIREMENTS
+# An instance with at least ~210 GiB of total GPU memory
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_id = "hugging-quants/Meta-Llama-3.1-405B-Instruct-AWQ-INT4"
+messages = [
+    {"role": "system", "content": "You are a pirate"},
+    {"role": "user", "content": "What's Deep Leaning?"},
+]
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    torch_dtype=torch.float16,
+    low_cpu_mem_usage=True,
+    device_map="auto",
+)
+
+inputs = tokenizer.apply_chat_template(
+    messages,
+    tokenize=True,
+    add_generation_prompt=True,
+    return_tensors="pt",
+    return_dict=True,
+).to("cuda")
+
+outputs = model.generate(**inputs, do_sample=True, max_new_tokens=256)
+print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
